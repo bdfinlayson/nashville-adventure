e1111614d4e4469ddbf7efd9e9f074e24130c7a9
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-20 22:03:07 -0500

    add player class and file

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,7 @@
+require_relative 'player'
+
 print "It is early morning. You wake up to the sound of birds chirping outside. You decide to:"
+
+response = gets.strip
+player = Player.new(response)
+print player.name
