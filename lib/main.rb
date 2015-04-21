44fae6408de38f8e207c3a1f62053eec2c3afee1
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-20 21:25:04 -0500

    ran first test. test passing

---
+++ lib/main.rb
@@ -0,0 +1,1 @@
+print "It is early morning. You wake up to the sound of birds chirping outside. You decide to:"
--- test/test_nashville_adventure.rb
+++ test/test_nashville_adventure.rb
@@ -3,7 +3,7 @@ require 'main.rb'
 
 class NashvilleAdventure < MiniTest::Unit::TestCase
   def test_with_no_arguments
-    output = `ruby main.rb`
+    output = `ruby lib/main.rb`
     expected = 'It is early morning. You wake up to the sound of birds chirping outside. You decide to:'
     assert_equal expected, output
   end
