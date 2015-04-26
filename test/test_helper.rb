62a6de823b8bd0a786961a9b991df4f8a46f2d18
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-26 13:17:09 -0500

    modify minitest configuration

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'minitest/reporters'
-require 'minitest/autorun'
-
-reporter_options = { color: true }
-
-Minitest::Reporters.use!
-[Minitest::Reporters::DefaultReporter.new(reporter_options)]
